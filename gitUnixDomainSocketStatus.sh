717b768e118fdff9d5621962c05624f0c5164809
John Schwartzman <john@fortesystem.com>
2018-07-16 19:40:49 -0400

    showing both forms

--- gitUnixDomainSocketStatus.sh
+++ gitUnixDomainSocketStatus.sh
@@ -1,110 +1,219 @@
 #!/bin/bash
 ##############################################################################
-# file: 			grep the output of a git status and determine whether
-#                   we need to commit any files (UnixDomainSocket project)
+# file: gitUnixDomainSocketStatus.sh
+#                   grep the output of a git status and determine whether
+#                   we need to commit any files or if we need to 
+#                   track untracked files (UnixDomainSocket project)
 # author: 			John Schwartzman, Forte Systems, Inc.
+#
 # last revision:	07/14/2018
 ##############################################################################
 
 set -o nounset			# use strict (no unset variables)
 
-cd /home/js/Development/UnixDomainSocket/utility/src
+strRed='\e[0;31m'       # define a few color escape sequences
+strGreen='\e[0;32m'
+strReset='\e[0m'
 
-COUNT1=$(git status --untracked-files=no | grep -c "nothing to commit")
-COUNT2=$(git status --untracked-files=normal | grep -c "Untracked files")
+strNoCommit="has no files to commit.${strReset}"
+strCommit="has 1 or more files that need to be committed.${strReset}"
+strNoUntracked="has no untracked files.${strReset}"
+strUntracked="has 1 or more untracked files.${strReset}"
 
-echo
-if [ $COUNT1 -eq 1 ];
-then
-    echo "utility has no files to commit.";
-fi
+cmd1="git status --untracked-files=no | grep -c \"nothing to commit\""
+cmd2="git status --untracked-files=normal | grep -c \"Untracked files\""
 
-if [ $COUNT2 -ne 0 ];
-then
-    echo "utility has 1 or more untracked files.";
-fi
-echo
+dirPrefix="/home/js/Development/UnixDomainSocket/"
+dirSuffix="/src/"
 
-cd /home/js/Development/UnixDomainSocket/transport/src
+# declare an array of the 6 project names
+declare -a prog[0]="utility" prog[1]="transport" prog[2]="serverTest" \
+           prog[3]="clientTest" prog[4]="simpleClientTest" \
+           prog[5]="utilityTest" prog[6]=""
+           
+numProg=6
 
-COUNT1=$(git status --untracked-files=no | grep -c "nothing to commit")
-COUNT2=$(git status --untracked-files=normal | grep -c "Untracked files")
-
-if [ $COUNT1 -eq 1 ];
-then
-    echo "transport has no files to commit.";
-fi
-
-if [ $COUNT2 -ne 0 ];
-then
-    echo "transport has 1 or more untracked files.";
-fi
+# the nProg directories consist of the dirPrefix + prog[index] + dirSuffix
 echo
 
+##############################################################################
+index=0
 
-cd /home/js/Development/UnixDomainSocket/clientTest/src
+# for each project in turn,
+#   cd to the project directory
+#   evaluate cmd1 and cmd2 and save the count of strings found in
+#   COUNT1 and COUNT2
 
-COUNT1=$(git status --untracked-files=no | grep -c "nothing to commit")
-COUNT2=$(git status --untracked-files=normal | grep -c "Untracked files")
+while [ ${index} -lt ${numProg} ] ; do
+    cd "${dirPrefix}${prog[${index}]}${dirSuffix}"
+    
+    COUNT1=`eval ${cmd1}`
+    COUNT2=`eval ${cmd2}`
 
-if [ $COUNT1 -eq 1 ];
+    if [ $COUNT1 -eq 1 ]
+    then
+        echo -e "${strGreen}${prog} $strNoCommit"
+    else
+        echo -e "${strRed}${prog} $strCommit"
+    fi
+
+    if [ $COUNT2 -ne 0 ]
+    then
+        echo -e "${strRed}${prog} $strUntracked"
+    else
+        echo -e "${strGreen}${prog} ${strNoUntracked}"
+    fi
+echo
+   
+    let index=index+1
+done
+
+exit 0
+
+
+##############################################################################
+prog="utility"
+dir="/home/js/Development/UnixDomainSocket/utility/src"
+cd $dir
+
+COUNT1=`eval ${cmd1}`
+COUNT2=`eval ${cmd2}`
+
+if [ $COUNT1 -eq 1 ]
 then
-    echo "clientTest has no files to commit.";
+    echo -e "${strGreen}${prog} $strNoCommit"
+else
+    echo -e "${strRed}${prog} $strCommit"
 fi
 
-if [ $COUNT2 -ne 0 ];
+if [ $COUNT2 -ne 0 ]
 then
-    echo "clientTest has 1 or more untracked files.";
+    echo -e "${strRed}${prog} $strUntracked"
+else
+    echo -e "${strGreen}${prog} ${strNoUntracked}"
+fi
+echo
+
+##############################################################################
+
+prog="transport"
+dir="/home/js/Development/UnixDomainSocket/transport/src"
+cd $dir
+
+COUNT1=`eval ${cmd1}`
+COUNT2=`eval ${cmd2}`
+
+if [ $COUNT1 -eq 1 ]
+then
+    echo -e "${strGreen}${prog} $strNoCommit"
+else
+    echo -e "${strRed}${prog} $strCommit"
+fi
+
+if [ $COUNT2 -ne 0 ]
+then
+    echo -e "${strRed}${prog} $strUntracked"
+else
+    echo -e "${strGreen}${prog} ${strNoUntracked}"
 fi
 echo
 
 
-cd /home/js/Development/UnixDomainSocket/simpleClientTest/src
+##############################################################################
 
-COUNT1=$(git status --untracked-files=no | grep -c "nothing to commit")
-COUNT2=$(git status --untracked-files=normal | grep -c "Untracked files")
+prog="clientTest"
+dir="/home/js/Development/UnixDomainSocket/clientTest/src"
+cd $dir
 
-if [ $COUNT1 -eq 1 ];
+COUNT1=`eval ${cmd1}`
+COUNT2=`eval ${cmd2}`
+
+if [ $COUNT1 -eq 1 ]
 then
-    echo "simpleClientTest has no files to commit.";
+    echo -e "${strGreen}${prog} $strNoCommit"
+else
+    echo -e "${strRed}${prog} $strCommit"
 fi
 
-if [ $COUNT2 -ne 0 ];
+if [ $COUNT2 -ne 0 ]
 then
-    echo "simpleClientTest has 1 or more untracked files.";
-fi
-echo
-
-
-cd /home/js/Development/UnixDomainSocket/serverTest/src
-
-COUNT1=$(git status --untracked-files=no | grep -c "nothing to commit")
-COUNT2=$(git status --untracked-files=normal | grep -c "Untracked files")
-
-if [ $COUNT1 -eq 1 ];
-then
-    echo "serverTest has no files to commit.";
-fi
-
-if [ $COUNT2 -ne 0 ];
-then
-    echo "serverTest has 1 or more untracked files.";
+    echo -e "${strRed}${prog} $strUntracked"
+else
+    echo -e "${strGreen}${prog} ${strNoUntracked}"
 fi
 echo
 
+##############################################################################
 
-cd /home/js/Development/UnixDomainSocket/utilityTest/src
+prog="simpleClientTest"
+dir="/home/js/Development/UnixDomainSocket/simpleClientTest/src"
+cd $dir
 
-COUNT1=$(git status --untracked-files=no | grep -c "nothing to commit")
-COUNT2=$(git status --untracked-files=normal | grep -c "Untracked files")
+COUNT1=`eval ${cmd1}`
+COUNT2=`eval ${cmd2}`
 
-if [ $COUNT1 -eq 1 ];
+if [ $COUNT1 -eq 1 ]
 then
-    echo "utilityTest has no files to commit.";
+    echo -e "${strGreen}${prog} $strNoCommit"
+else
+    echo -e "${strRed}${prog} $strCommit"
 fi
-if [ $COUNT2 -ne 0 ];
+
+if [ $COUNT2 -ne 0 ]
 then
-    echo "utilityTest has 1 or more untracked files.";
+    echo -e "${strRed}${prog} $strUntracked"
+else
+    echo -e "${strGreen}${prog} ${strNoUntracked}"
 fi
 echo
+
+##############################################################################
+
+prog="serverTest"
+dir="/home/js/Development/UnixDomainSocket/serverTest/src"
+cd $dir
+
+COUNT1=`eval ${cmd1}`
+COUNT2=`eval ${cmd2}`
+
+if [ $COUNT1 -eq 1 ]
+then
+    echo -e "${strGreen}${prog} $strNoCommit"
+else
+    echo -e "${strRed}${prog} $strCommit"
+fi
+
+if [ $COUNT2 -ne 0 ]
+then
+    echo -e "${strRed}${prog} $strUntracked"
+else
+    echo -e "${strGreen}${prog} ${strNoUntracked}"
+fi
+echo
+
+##############################################################################
+
+prog="utilityTest"
+dir="/home/js/Development/UnixDomainSocket/utilityTest/src"
+cd $dir
+
+COUNT1=`eval ${cmd1}`
+COUNT2=`eval ${cmd2}`
+
+if [ $COUNT1 -eq 1 ]
+then
+    echo -e "${strGreen}${prog} $strNoCommit"
+else
+    echo -e "${strRed}${prog} $strCommit"
+fi
+
+if [ $COUNT2 -ne 0 ]
+then
+    echo -e "${strRed}${prog} $strUntracked"
+else
+    echo -e "${strGreen}${prog} ${strNoUntracked}"
+fi
+echo
+
+##############################################################################
 
